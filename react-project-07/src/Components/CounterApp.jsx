e72bb7091ca272f0532bfb83a68064cdc1d8ee6d
mrjawadhu <kk2477048@gmail.com>
2025-05-19 23:43:46 +0530

    switch case

--- react-project-07/src/Components/CounterApp.jsx
+++ react-project-07/src/Components/CounterApp.jsx
@@ -5,6 +5,8 @@ import styled from "styled-components";
 const CounterApp = () => {
   let[count, setCounts] = useState(0)
 
+  const [data ,setdata]=useState("Loading")
+
   
   
   
@@ -26,6 +28,49 @@ let [sample1,setSample]=useState(()=>{return hfun()})
     
     return 100
   }
+let message;
+let remainingClicks=10-count;
+
+if(count<10){
+  message =(
+    <div>
+      <h3>You clicked {count} times</h3>
+      <p>Still {remainingClicks}more times to reach a 10% discount🔒</p>
+    </div>
+  )
+
+}
+else if(count===10) {
+  message=(
+    <div>
+    <h3>You clicked {count} times</h3>
+    <p>You unlocked a 10% discount🛒</p>
+    </div>
+  )
+}
+else if(count<20){
+  message=(
+    <div>
+      <h3>you clicked {count} times</h3>
+      <p>you're on the way to get more rewrds! keep click 20% discount🔒</p>
+    </div>
+  )
+}
+else{
+  message=(
+    <div>
+      <h3>you clicked {count} times</h3>
+      <p>you're reach a top reward! you are a click master❤</p>
+    </div>
+  )
+}
+let DataComponent=()=>{
+switch(data){
+  case "Loading":return <LoadingComponent/>
+  case "Succes":return <SuccesComponent/>
+  case "Error": return<ErrorComponent/>
+}
+}
   return( 
   
   <div>
@@ -44,17 +89,38 @@ let [sample1,setSample]=useState(()=>{return hfun()})
         // display:"flex"
       }} className="btn">Decrease</button>
 
-      {
+      {/* {
         count>=10 ? ( <p>Your unlocked a 10% discount🛒</p>  ):(<p>Click to 10 times unlock your Reward🔒</p>)
       }
       {
         count>=20 && <p>You're a click master ❤</p>
       }
-      {count>=21 && <p>Nice bro Make a some money🤑</p>}
-
+      {count>=21 && <p>Nice bro Make a some money🤑</p>} */}
+{
+  message
+}
+{
+DataComponent()
+}
   </div>
-  
+
 )
 };
 
 export default CounterApp;
+
+function LoadingComponent(){
+  return(
+    <h4>Loadind...</h4>
+  )
+}
+function SuccesComponent(){
+  return(
+    <h4>
+      succes🧡
+    </h4>
+  )
+}
+function ErrorComponent(){
+  return <h4>Error😴</h4>
+}
